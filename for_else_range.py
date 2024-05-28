26d2df976ccf254483b57c4227335147d400a02c
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-05-28 11:05:10 +0530

    Add files via upload

---
+++ for_else_range.py
@@ -0,0 +1,5 @@
+# for loop using else etatements
+for x in range(6):                           # for 0 to 6 number range
+    print(x)                                 # print 0-6 number
+else:                                        # using else statement final or last execution
+    print("Finally finished")
